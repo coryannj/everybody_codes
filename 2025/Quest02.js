d9fa30640af3a9ab7b14820366875c30824a7b87
Cory-Ann Joseph <coryannj@gmail.com>
2025-11-04 17:34:03 +1100

    Q2 setup

---
+++ 2025/Quest02.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const input1 = fs.readFileSync('../inputs/everybody_codes/2025/quest2_1.txt',{ encoding: 'utf8', flag: 'r' });
+const input2 = fs.readFileSync('../inputs/everybody_codes/2025/quest2_2.txt',{ encoding: 'utf8', flag: 'r' });
+const input3 = fs.readFileSync('../inputs/everybody_codes/2025/quest2_3.txt',{ encoding: 'utf8', flag: 'r' });
+
