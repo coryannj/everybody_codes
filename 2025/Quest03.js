01fdef88ac13f4b3791b43c86f1e68aa0afe03f8
coryannj-anz <174399716+coryannj-anz@users.noreply.github.com>
2025-11-06 19:45:24 +1100

    Q3 solution

--- 2025/Quest03.js
+++ 2025/Quest03.js
@@ -3,3 +3,8 @@ const input1 = fs.readFileSync('../inputs/everybody_codes/2025/quest3_1.txt',{ e
 const input2 = fs.readFileSync('../inputs/everybody_codes/2025/quest3_2.txt',{ encoding: 'utf8', flag: 'r' });
 const input3 = fs.readFileSync('../inputs/everybody_codes/2025/quest3_3.txt',{ encoding: 'utf8', flag: 'r' });
 
+console.log([...new Set(input1.split(',').map(Number))].reduce((a,c)=>a+c,0)) // Part 1
+
+console.log([...new Set(input2.split(',').map(Number))].sort((a,b) => b-a).slice(-20).reduce((a,c)=>a+c,0)) // Part 2
+
+console.log(Math.max(...Object.values(input3.split(',').map(Number).reduce((a, c) => {return a[c] ? ++a[c] : a[c] = 1, a}, {})))) // Part 3 - shameful rewrite after using for loop originally lolsob
