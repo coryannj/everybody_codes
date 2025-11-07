48a2e8399496035a50cdc6bd8b81f5a877af4134
coryannj-anz <174399716+coryannj-anz@users.noreply.github.com>
2025-11-07 18:00:15 +1100

    Q4 solution

---
+++ 2025/Quest04.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input1 = fs.readFileSync('../inputs/everybody_codes/2025/quest4_1.txt',{ encoding: 'utf8', flag: 'r' });
+const input2 = fs.readFileSync('../inputs/everybody_codes/2025/quest4_2.txt',{ encoding: 'utf8', flag: 'r' });
+const input3 = fs.readFileSync('../inputs/everybody_codes/2025/quest4_3.txt',{ encoding: 'utf8', flag: 'r' });
+
+const turns = (input,firstRotations) => {
+    gears = input.split(/[\r\n]+/).map((x)=>x.split("|").map(Number))
+
+    let baseTurns = gears.map((x,i,a) => i===0 ? 1 : a[i-1].at(-1)/x[0]);
+
+    return baseTurns.reduce((a,c)=>a*c,firstRotations)
+} 
+
+console.log(Math.floor(turns(input1,2025))) // Part 1
+
+console.log(Math.ceil(10000000000000/turns(input2,1))) // Part 2
+
+console.log(Math.floor(turns(input3,100))) // Part 3
