4cbf3f16a077950f7d7277d99f2e780a1d392c6d
Cory-Ann Joseph <coryannj@gmail.com>
2025-11-08 12:06:05 +1100

    Q5 solution

--- 2025/Quest05.js
+++ 2025/Quest05.js
@@ -3,17 +3,41 @@ const input1 = fs.readFileSync('../inputs/everybody_codes/2025/quest5_1.txt',{ e
 const input2 = fs.readFileSync('../inputs/everybody_codes/2025/quest5_2.txt',{ encoding: 'utf8', flag: 'r' });
 const input3 = fs.readFileSync('../inputs/everybody_codes/2025/quest5_3.txt',{ encoding: 'utf8', flag: 'r' });
 
-//input.split(/[\r\n]+/)
-const turns = (input,firstRotations) => {
-    gears = input.split(/[\r\n]+/).map((x)=>x.split("|").map(Number))
+const allQualities = (input) => {
+    let swords = input.split(/[\r\n]+/).map((x)=>x.split(':').map((y,i)=>i===0?parseInt(y):y.split(',').map(Number)))
+    
+    const quality = ([id,nums]) => {
+        let sword = []
+        for (const n of nums){
+            let leftIndex = sword.findIndex(([l,c,r])=> n<c && l=== -1)
+            let rightIndex = sword.findIndex(([l,c,r])=> n>c && r === -1)
+            if(leftIndex !== -1){
+                sword[leftIndex][0]=n
+            } else if (rightIndex !== -1){
+                sword[rightIndex][2]=n
+            } else {
+                sword.push([-1,n,-1])
+            }
+        }
 
-    let baseTurns = gears.map((x,i,a) => i===0 ? 1 : a[i-1].at(-1)/x[0]);
+        return {
+            "id": id,
+            "quality":parseInt(sword.map((x)=>x[1]).join('')),
+            "fishbone":sword.map((x)=>parseInt(x.filter((y)=>y>=0).join('')))
+        }
+    }
+    
+    return swords.map(quality).sort((a,b)=>{
+        if(a.quality===b.quality){
+            let firstDiff = a.fishbone.findIndex((x,i)=> x !== b.fishbone[i])
+            return firstDiff === -1 ? b.id-a.id: b.fishbone[firstDiff]-a.fishbone[firstDiff]
+        } else {
+            return b.quality-a.quality
+        }
+    })
+}
 
-    return baseTurns.reduce((a,c)=>a*c,firstRotations)
-} 
+console.log(allQualities(input1)[0].quality) // Part 1
 
-console.log(Math.floor(turns(input1,2025))) // Part 1
-
-console.log(Math.ceil(10000000000000/turns(input2,1))) // Part 2
-
-console.log(Math.floor(turns(input3,100))) // Part 3
+console.log(allQualities(input2).map((x)=>x.quality).filter((x,i,a)=>i===0 || i===a.length-1).reduce((a,c)=>a-c)) // Part 2
+console.log(allQualities(input3).map((x,i)=>x.id*(i+1)).reduce((a,c)=>a+c)) // Part 3
