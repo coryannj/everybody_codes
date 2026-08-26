9bc85aa3cd4a39b491af1da0b28eaac86d0d05f4
Cory-Ann Joseph <coryannj@gmail.com>
2026-08-26 17:23:10 +1000

    Merged p2 and p3 increment loop

--- 2026/the_digital_atelier/Quest01.js
+++ 2026/the_digital_atelier/Quest01.js
@@ -3,34 +3,32 @@ const input1 = fs.readFileSync('../../inputs/everybody_codes/2026/the_digital_at
 const input2 = fs.readFileSync('../../inputs/everybody_codes/2026/the_digital_atelier/quest1_2.txt',{ encoding: 'utf8', flag: 'r' });
 const input3 = fs.readFileSync('../../inputs/everybody_codes/2026/the_digital_atelier/quest1_3.txt',{ encoding: 'utf8', flag: 'r' });
 
-const noOverlap = ([x1,y1],[x2,y2]) => x1 < x2 && y1 > y2 || x2 < x1 && y2 > y1 || x2 > y1 || y2 < x1
+const noOverlap = ([x1,y1],[x2,y2]) => x1 < x2 && y2 < y1 || x2 < x1 && y1 < y2 || x2 > y1 || y2 < x1
 
 const goBackwards = (curr,jump,seen,jumps,partNo) => {
-    return curr-jump > 0 && !seen.has(curr-jump) && (partNo < 3 || jumps.length === 0 || jumps.every((j)=> noOverlap(j,[curr-jump,curr]))) ? [true,curr-jump] : [false,-1]
+    return curr-jump > 0 && !seen.has(curr-jump) && (partNo < 3 || jumps.length === 0 || !jumps.some((j)=> !noOverlap(j,[curr-jump,curr]))) ? curr-jump : undefined
 }
 
 const goForwards = (curr,jump,seen,jumps,partNo) => {
-    if(partNo === 1 || (partNo === 2 && !seen.has(curr+jump)) || (partNo === 3 && jumps.length === 0)) return [true,curr+jump]
+    const loopTest = (partNo) => partNo === 2 ? seen.has(curr+jump) : seen.has(curr+jump) || jumps.some(([a,b])=>!noOverlap([a,b],[curr,curr+jump]))
 
-    if(partNo === 2){
-        while(seen.has(curr+jump)){
+    if(partNo === 1 || !loopTest(partNo)) return curr+jump
+
+    let max
+    let newCurr
+
+    if(partNo === 2 || !jumps.some((j)=>j.includes(curr))){
+        max = partNo === 2 ? Infinity : jumps.findLast(([a,b])=> a < curr && b > curr)?.[1] || Infinity
+
+        while(loopTest(partNo)){
             jump++
+            if(curr+jump >= max) break;
         }
-        return [true,curr+jump]
-    } else {
-        if(!jumps.some((j)=>j.includes(curr))){
-            let max = jumps.findLast(([a,b])=> a < curr && b > curr)?.[1] || Infinity
 
-            while(seen.has(curr+jump) || jumps.some(([a,b])=>!noOverlap([a,b],[curr,curr+jump]))){
-                jump++
-                if(curr+jump >= max) break;
-            }
-
-            if(curr+jump<max) return [true,curr+jump]
-        }
-        
-        return [false,-1]
+        if(curr+jump<max) newCurr = curr+jump
     }
+
+    return newCurr
 }
 
 const solve = (input,partNo) => {
@@ -43,15 +41,10 @@ const solve = (input,partNo) => {
         let seen = new Set([curr])
 
         for (const jump of line){
-            let canGoBack,canGoForward,newCurr
 
-            [canGoBack,newCurr] = goBackwards(curr,jump,seen,allJumps[count%2],partNo)
+            let newCurr = goBackwards(curr,jump,seen,allJumps[count%2],partNo) || goForwards(curr,jump,seen,allJumps[count%2],partNo)
 
-            if(!canGoBack){
-                [canGoForward,newCurr] = goForwards(curr,jump,seen,allJumps[count%2],partNo)
-            }
-
-            if(canGoBack || canGoForward){
+            if(newCurr){
                 allJumps[count%2].push([curr,newCurr].sort((a,b)=>a-b))
                 allJumps[count%2].sort(([x1,y1],[x2,y2])=>x1-x2)
                 seen.add(newCurr)
@@ -59,7 +52,7 @@ const solve = (input,partNo) => {
                 count++
             }
         }
-        //console.log(curr)
+
         total+=curr
     }
     
